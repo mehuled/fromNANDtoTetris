62597d56411595d7e3e8f1f314d49237fcfb69c8
Mehul Sharma <mehulcse0209@gmail.com>
2020-01-19 04:55:14 +0530

    Completed project/04

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,40 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@1
+D=A
+@i
+M=D      // i = 1
+@0
+D=A
+@sum
+M=D      //sum = 0 
+
+(LOOP)
+@i
+D=M
+@R0
+D=D-M
+@STOP
+D;JGT
+
+@R1
+D=M
+@sum
+M=D+M
+
+@i
+M=M+1
+@LOOP
+0;JEQ
+(STOP)
+@sum
+D=M
+@R2
+M=D
+
+(END)
+@END
+0 ; JEQ
+
